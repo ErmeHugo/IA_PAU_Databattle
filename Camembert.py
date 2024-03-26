90f4868678d6105d858131629c2c7486ceb0ce6d
ErmeHugo <sembinelli@cy-tech.fr>
2024-03-26 15:50:43 +0100

    Ajout Model Similarité Semantique et ses fonctions

---
+++ Camembert.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import mysql.connector
+import re
+from sql_info import *
+from Fonction_Camembert import *
+
+
+########### LIAISON BDD PYTHON ##############
+
+
+# Connexion de la database aux mysql perso
+connection = mysql.connector.connect(
+    host=localhost,
+    user=root, 
+    password=pwd,  
+    database=db
+)
+cursor = connection.cursor()
+
+table = "tbldictionnaire"
+colonnes = "codeappelobjet,traductiondictionnaire"
+conditions = f"codelangue = 2 AND typedictionnaire = 'sol' AND indexdictionnaire = 1"#AND codeappelobjet = 719
+
+requete = f"SELECT {colonnes} FROM {table} WHERE {conditions}"
+
+# Execution de la requête
+try:
+    cursor.execute(requete)
+    results = cursor.fetchall()
+    dict_sol = {}
+    for row in results:
+        # En considérant que le premier terme est le codeappelobjet
+        # Création d'un dictionnaire reliant solution à son texte 
+        if row[0] in dict_sol :
+            dict_sol[row[0]].append(row[1])
+        else : 
+            dict_sol[row[0]] = [row[1]]
+    for key,item in dict_sol.items():
+        text = " ".join(item)
+        dict_sol[key] = text
+
+except mysql.connector.Error as error:
+    print("Error executing SQL query:", error)
+
+finally:
+    cursor.close()
+    connection.close()
+
+
+########### PRÉTRAITEMENT BDD ##############
+
+
+
+for key,text in dict_sol.items():
+    #text = remove_punctuation(text)
+    text = remove_unnecessary_tokens(text)
+    lemmatized_text = custom_lemmatize(text)
+    dict_sol[key] = ""
+    dict_sol[key] = " ".join(lemmatized_text)
+
+
+
+########### MODEL  ##############
+
+model =  SentenceTransformer("dangvantuan/sentence-camembert-large")
+
+# Charger les embeddings à partir du fichier
+# calculate_and_save_embeddings(model, dict_sol)
+dict_embeddings = joblib.load('embeddings.pkl')
+
+# Utilisation de la fonction pour trouver les textes les plus pertinents pour une question spécifique
+question = "C'est quoi la HP flottante ?"
+find_top_texts(question, model, dict_embeddings, dict_sol)
+
+question_2 = "Je voudrais dimensionner un panneau solaire."
+find_top_texts(question_2, model, dict_embeddings, dict_sol)
+
+question_3 = "Quel gain pour un variateur de vitesse ?"
+find_top_texts(question_3, model, dict_embeddings, dict_sol)
+
+question_4 = "Quelles sont les meilleures solutions pour l'agro-alimentaire ? "
+find_top_texts(question_4, model, dict_embeddings, dict_sol)
+
--- read_sql.py
+++ read_sql.py
@@ -26,7 +26,7 @@ cursor = connection.cursor()
 
 table = "tbldictionnaire"
 colonnes = "traductiondictionnaire,codeappelobjet"
-conditions = f"codelangue = 2 "#AND codeappelobjet = 719"
+conditions = f"codelangue = 2 AND typedictionnaire = 'sol' AND indexdictionnaire = 1"
 
 
 requete = f"SELECT {colonnes} FROM {table} WHERE {conditions}"
@@ -40,61 +40,7 @@ nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-from tqdm import tqdm
-import os
 
-stop_words =set(stopwords.words('french'))
-
-
-# suppresion de la ponctuation, passage en minuscule, stemming, lemmatisation, stopwords
-
-def clean_tweet(text, flg_stemm=False, flg_lemm=True, lst_stopwords=None):
-    """
-    Cette fonction va nettoyer le texte :
-    + mettre en minuscule,
-    + supprimer un certain nombre d'expression,
-    + choix lemmatisation / stemming,
-    + choix d'une liste de stopwords
-    """
-
-    text = text.lower()
-    text = text.replace('\n', ' ').replace('\r', '')
-    text = ' '.join(text.split())
-    text = re.sub(r"[A-Za-z\.]*[0-9]+[A-Za-z%°\.]*", "", text)
-    text = re.sub(r"(\s\-\s|-$)", "", text)
-    text = re.sub(r"[,\!\?\%\(\)\/\"]", "", text)
-    text = re.sub(r"\&\S*\s", "", text)
-    text = re.sub(r"\&", "", text)
-    text = re.sub(r"\+", "", text)
-    text = re.sub(r"\#", "", text)
-    text = re.sub(r"\$", "", text)
-    text = re.sub(r"\£", "", text)
-    text = re.sub(r"\%", "", text)
-    text = re.sub(r"\:", "", text)
-    text = re.sub(r"\@", "", text)
-    text = re.sub(r"\-", "", text)
-
-    ## transforme en tokens
-    lst_text = text.split()
-    ## remove Stopwords
-    if lst_stopwords is not None:
-        lst_text = [word for word in lst_text if word not in
-
-                    lst_stopwords]
-
-    ## Stemming (supprime -ing, -ly, ...)
-    if flg_stemm == True:
-        ps = nltk.stem.porter.PorterStemmer()
-        lst_text = [ps.stem(word) for word in lst_text]
-
-    ## Lemmatisation (retourne la racine du mot)
-    if flg_lemm == True:
-        lem = nltk.stem.wordnet.WordNetLemmatizer()
-        lst_text = [lem.lemmatize(word) for word in lst_text]
-
-    ## liste => string
-    text = " ".join(lst_text)
-    return text
 
 
 # Execution de la requête
@@ -109,8 +55,6 @@ try:
     print(row_list)"""
     for row in results:
         row_list = list(row)
-        row_list[0] = remove_unnecessary_tokens(row_list[0])
-        row_list[0] = clean_tweet(row_list[0], flg_stemm=False, flg_lemm=True, lst_stopwords=None)
         # Fonctionne en considérant que la première requête est un texte 
         # Possibilité de travailler avec des dico pour palier à ça
         print(row_list[0])
@@ -122,37 +66,3 @@ finally:
     cursor.close()
     connection.close()
 
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
- 
- 
-def extract_keywords(text):
-    # Tokenisation du texte
-    tokens = word_tokenize(text)
-    # Étiquetage des parties du discours
-    pos_tags = pos_tag(tokens)
-    # Extraction des mots-clés (noms et verbes)
-    keywords = [word for word, pos in pos_tags if pos.startswith('N') or pos.startswith('V')]
-    return keywords
-""" 
-# Exemple d'utilisation
-text = "atelier"
-keywords = extract_keywords(text)
-print("Mots-clés :", keywords)    
-
-for i in range(len(row_list)):
-    row_list[i][0] = extract_keywords(row_list[i][0])
-#print(row_list)
-
-renvoyer = []
-
-for i in range(len(row_list)):
-    for j in range(len(keywords)):
-        for k in range(len(row_list[i][0])):
-            if keywords[j] == row_list[i][0][k]:
-                renvoyer.append(row_list[i])
-                print(row_list[i],"\n")
-"""
